4793c4e23c0bf77e3d681afc54927dedf914f35a
Ali Sheikh <MISD_uskt@outlook.com>
2021-02-01 16:18:50 +0500

    Add project files.

---
+++ INV MS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace INV_MS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
