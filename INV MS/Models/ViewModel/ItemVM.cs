8b4bfd9a87957bd5914958cf2cf00e0ad37320cb
ALI <ALI@DESKTOP-LI9BNBL>
2021-02-19 22:37:58 +0500

    Add ViewModel Of Item + Remove the Jquery Code and Paste in Site.Js File + Change Something in Accounts Index Page

---
+++ INV MS/Models/ViewModel/ItemVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INV_MS.Models.ViewModel
+{
+    public class ItemVM
+    {
+        public int itemId { get; set; }
+        public int catId { get; set; }
+        public int UnitId { get; set; }
+        public string ItemCode { get; set; }
+        public string itemName { get; set; }
+        public string Description { get; set; }
+        public decimal purchase_Price { get; set; }
+        public decimal sale_Price { get; set; }
+    }
+}
--- INV MS/wwwroot/js/site.js
+++ INV MS/wwwroot/js/site.js
@@ -1,4 +1,125 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+////Account Code Start
+//Submit the Form
+$('#save').click(function () {
+    var AccountHeadName = $('.acchl').val();
+    var AccountCode = $('.accc').val();
+    var AccountTitle = $('.acct').val();
+    var PhoneNo = $('.pn').val();
+    var MobileNo = $('.mn').val();
+    var Email = $('.em').val();
+    var Address = $('.add').val();
+
+    $.ajax({
+        url: "/Accounts/Create",
+        type: "POST",
+        dataType: 'json',
+        data: {
+            "accountHeadId": AccountHeadName,
+            "accountCode": AccountCode,
+            "accountTitle": AccountTitle,
+            "PhoneNo": PhoneNo,
+            "MobileNo": MobileNo,
+            "Email": Email,
+            "Address": Address
+        }, success: function (output) {
+
+
+            $("#table_id").load(window.location.href + " #tableid");
+            //location.reload();
+
+        },
+        error: function () {
+            alert("Result is = " + data);
+
+        }
+
+    });
+
+});
+//Toggle Account Detail Form
+function ShowAccDetail() {
+    $('#content').toggle('show');
+
+}
+function refresh() {
+    Location.refresh();
+}
+
+
+function deletes(Id) {
+    $.ajax({
+        type: "POST",
+        url: "/Accounts/Delete/?" + Id,
+        dataType: "json",
+        data: { Id: Id },
+        success: (function (res) {
+            alert("hy=" + data);
+
+        }),
+        error: (function () { alert("Error") })
+    });
+}
+
+
+
+////Account Code End
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write your JavaScript code.
+
+$('#save').click(function () {
+    var Category = $('.cat').val();
+    var itmcode = $('.itmcode').val();
+    var itemName = $('.itemName').val();
+    var unit = $('.unit').val();
+    var Description = $('.Description').val();
+    var purchase_Price = $('.purchase_Price').val();
+    var sale_Price = $('.sale_Price').val();
+
+    $.ajax({
+        url: "/Item/Item",
+        type: "POST",
+        dataType: 'json',
+        data: {
+            "catId": Category,
+            "ItemCode": itmcode,
+            "itemName": itemName,
+            "UnitId": unit,
+            "Description": Description,
+            "purchase_Price": purchase_Price,
+            "sale_Price": sale_Price
+        }, success: function (output) {
+
+            alert(output);
+           
+           
+
+        },
+        error: function (output) {
+            alert("Result is = " + output);
+
+        }
+
+    });
+
+});
+
+
+
+
